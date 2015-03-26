ee24f366929cb5605dd8c96a0f971186dc69f55b
Guilherme Louro <guilherme-louro@hotmail.com>
2015-03-26 20:31:22 -0300

    new tags for angular in template and 'fields' in items

--- simpleajs/core/static/js/controlles.js
+++ simpleajs/core/static/js/controlles.js
@@ -1,9 +1,15 @@
 var app = angular.module("myApp", []);
 
+app.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{$');
+  $interpolateProvider.endSymbol('$}');
+});
+
 app.controller("customersCtrl", function($scope, $http) {
   $http.get('/getdata/').
     success(function(data, status, headers, config) {
       $scope.posts = data;
+      console.log($scope.posts);
     }).
     error(function(data, status, headers, config) {
       // log error
